91fa906c2aa55e34a8c92bd9ab9be3e37a4e959b
Anshika <sanshikas010@gmail.com>
2023-11-09 05:08:30 +0530

    Added loading state,

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:booksapp_asign/services/fetch_data.dart';
 import 'package:flutter/material.dart';
 
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -30,33 +31,55 @@ class _HomePageState extends State<HomePage> {
         title: Text("Listexample"),
         backgroundColor: Colors.blue[900],
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-         ListTile(
-           title: Text("Item 1"),
-           subtitle: Text("Sub Item 1"),
-         ),
-          ListTile(
-            title: Text("Item 2"),
-            subtitle: Text("Sub Item 2"),
-          ),
-          ListTile(
-            title: Text("Item 3"),
-            subtitle: Text("Sub Item 3"),
-          ),
-          ListTile(
-            title: Text("Item 4"),
-            subtitle: Text("Sub Item 4"),
-          ),
-          ListTile(
-            title: Text("Item 5"),
-            subtitle: Text("Sub Item 5"),
-          ),
-        ],
+      body: Align(
+        alignment: Alignment.center,
 
-
-      ),
+          child: bodyUI(),
+        ),
     );
+  }
+  Widget bodyUI() {
+
+    return FutureBuilder<Map<String, dynamic>>(
+      future: FetchData.instance.getData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Data is being fetched, show a loading indicator.
+          return const Align(alignment: Alignment.center, child: CircularProgressIndicator(),);
+        } else if (snapshot.hasError) {
+          // An error occurred, display an error message.
+          return Text('Error: ${snapshot.error}');
+        } else {
+          // Data has been fetched, display it.
+          final data = snapshot.data;
+          return  ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              ListTile(
+                title: Text("Item 1"),
+                subtitle: Text("Sub Item 1"),
+              ),
+              ListTile(
+                title: Text("Item 2"),
+                subtitle: Text("Sub Item 2"),
+              ),
+              ListTile(
+                title: Text("Item 3"),
+                subtitle: Text("Sub Item 3"),
+              ),
+              ListTile(
+                title: Text("Item 4"),
+                subtitle: Text("Sub Item 4"),
+              ),
+              ListTile(
+                title: Text("Item 5"),
+                subtitle: Text("Sub Item 5"),
+              ),
+            ],
+          );
+        }
+      },
+    );
+
   }
 }
--- lib/services/fetch_data.dart
+++ lib/services/fetch_data.dart
@@ -1,13 +1,19 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-class FetchData{
+
+
+class FetchData {
+
+
 
   static FetchData instance = FetchData();
-  Future<dynamic> getData() async {
+
+  Future<Map<String, dynamic>> getData() async {
     var response = await http.post(
-      Uri.parse( "https://api.thenotary.app/lead/getLeads"),
+      Uri.parse("https://api.thenotary.app/lead/getLeads"),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode(<String, String>{
         "notaryId": "643074200605c500112e0902",
@@ -15,14 +21,10 @@ class FetchData{
     );
 
     if (response.statusCode == 200) {
-      // If the server returns a 200 OK response,
-      // then parse the JSON.
-      return response.body;
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data;
     } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to post data');
+      throw Exception('Failed to fetch data from the API');
     }
   }
-
 }
